45b3c5ae48406ccdad6817d4b3b2a7373d19afc2
ace <15368104568@189.cn>
2024-11-13 14:49:30 +0800

    Update Logic.vb

--- Logic.vb
+++ Logic.vb
@@ -4,6 +4,12 @@ Declare Module y1
 Declare Module y2
 Declare Module z1
 Declare Module z2
+Function hwd
+        char height = h
+        char width = w
+        char depth = d
+End Function
+            
 Sub XYZ
   If x1 = 0 Then height + 1
   Else x2 = 0 Then height - 1
@@ -12,6 +18,7 @@ Sub XYZ
   Else z1 = 0 Then depth + 1
   Else z2 = 0 Then depth - 1
 End Sub
+              
     Dim x1 As Integer
     Dim y1 As Float
     Dim z1 As Double
@@ -19,5 +26,4 @@ Next
     ReDim x2 As Integer
     ReDim y2 As Float
     ReDim z2 As Double
-
-  
+Const (x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2 = 1
