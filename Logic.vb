9f6d5355ad62bbfa46bea84e635e452d5a9b48b6
ace <15368104568@189.cn>
2024-11-13 14:46:49 +0800

    Update Logic.vb

--- Logic.vb
+++ Logic.vb
@@ -1,1 +1,23 @@
+Declare Module x1
+Declare Module x2
+Declare Module y1
+Declare Module y2
+Declare Module z1
+Declare Module z2
+Sub XYZ
+  If x1 = 0 Then height + 1
+  Else x2 = 0 Then height - 1
+  Else y1 = 0 Then width + 1
+  Else y2 = 0 Then width - 1
+  Else z1 = 0 Then depth + 1
+  Else z2 = 0 Then depth - 1
+End Sub
+    Dim x1 As Integer
+    Dim y1 As Float
+    Dim z1 As Double
+Next
+    ReDim x2 As Integer
+    ReDim y2 As Float
+    ReDim z2 As Double
 
+  
